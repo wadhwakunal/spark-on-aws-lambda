1ef7f7509fd662885d6be86978b3b221c96cbfc9
Kunal Wadhwa <kunal.wadhwa@ballebaazi.com>
2024-12-27 21:12:38 +0530

    Cleaning code for Spark on Lambda Framework

--- sparkLambdaHandler.py
+++ sparkLambdaHandler.py
@@ -17,26 +17,6 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 #handler.setFormatter(formatter)
 #logger.addHandler(handler)
             
-def get_unprocessed_files(s3_bucket_script: str,unprocessed_file_key: str) -> str:
-    try:
-        logger.info('Inside function get_unprocessed_files')
-        s3 = boto3.resource("s3")
-        content = s3.Object(s3_bucket_script, unprocessed_file_key).get()['Body'].read().decode('utf-8')
-        logger.info(f'Unprocessed files: {content}')
-        logger.info(f'Now deleting file {unprocessed_file_key}')
-        s3.Object(s3_bucket_script, unprocessed_file_key).delete()
-        return content
-    except botocore.exceptions.ClientError as e:
-        logger.error(f"Boto Error: {e.response['Error']['Code']}")
-        sys.exit(0)
-        #raise e
-    except Exception as e :
-        logger.error(f"Error: {e}")
-        sys.exit(0)
-        #raise e
-    else:
-        logger.info(f'Successfully extracted file names from {unprocessed_file_key}')
-    
 def s3_script_download(s3_bucket_script: str,input_script: str)-> None:
     """
     """
@@ -49,60 +29,6 @@ def s3_script_download(s3_bucket_script: str,input_script: str)-> None:
         raise e
     else:
         logger.info(f'Script {input_script} successfully downloaded to /tmp')
-
-def write_content_to_s3_file(s3_bucket: str,s3_key: str,content: str) -> None:
-    logger.info(f"Writing content to file {s3_key}")
-    try:
-        s3_client = boto3.resource("s3")
-        s3_client.Bucket(s3_bucket).put_object(Key=s3_key, Body=content.encode("utf-8"))
-    except botocore.exceptions.ClientError as e:
-        logger.error(f"Boto Error: {e.response['Error']['Code']}")
-        raise e
-    except Exception as e :
-        logger.error(f"Error: {e}")
-        raise e
-    else:
-        logger.info(f'Successfully written content to file {s3_key}')
-
-def read_content_from_s3_file(s3_bucket: str,s3_key: str) -> str:
-    logger.info(f"Reading content from file {s3_key}")
-    try:
-        s3_client = boto3.resource("s3")
-        content = s3_client.Object(s3_bucket, s3_key).get()['Body'].read().decode('utf-8')
-        return content
-    except botocore.exceptions.ClientError as e:
-        logger.error(f"Boto Error: {e.response['Error']['Code']}")
-        return ""
-    except Exception as e :
-        logger.error(f"Error: {e}")
-        return ""
-    else:
-        logger.info(f'Successfully read content from file {s3_key}')
-
-def delete_file_from_s3(s3_bucket: str,s3_key: str) -> None:
-    logger.info(f"Deleting file {s3_key}")
-    try:
-        s3_client = boto3.resource("s3")
-        s3_client.Object(s3_bucket, s3_key).delete()
-    except botocore.exceptions.ClientError as e:
-        logger.error(f"Boto Error: {e.response['Error']['Code']}")
-    except Exception as e :
-        logger.error(f"Error: {e}")
-    else:
-        logger.info(f'Successfully deleted file {s3_key}')
-
-def load_partitions(database_name: str,table_name: str,athena_workgroup: str) -> None:
-    logger.info(f'Loading partitions of table: {database_name}.{table_name}')
-    try:
-        athena_client = boto3.client("athena")
-        query = f'MSCK REPAIR TABLE {database_name}.{table_name}'
-        athena_client.start_query_execution(QueryString=query,QueryExecutionContext={'Database': database_name},WorkGroup=athena_workgroup)
-    except botocore.exceptions.ClientError as e:
-        logger.error(f"Boto Error: {e.response['Error']['Code']}")
-    except Exception as e :
-        logger.error(f"Error: {e}")
-    else:
-        logger.info(f'Successfully loaded partitions in table {database_name}.{table_name}')
 
 def spark_submit(s3_bucket_script: str,input_script: str, event: dict)-> None:
     """
@@ -119,17 +45,12 @@ def spark_submit(s3_bucket_script: str,input_script: str, event: dict)-> None:
     # Run the spark-submit command on the local copy of the script
     try:
         logger.info(f'Spark-Submitting the Spark script {input_script} from {s3_bucket_script}')
-        subprocess.run(["spark-submit", "/tmp/spark_script.py", "--event", json.dumps(event)], check=True, env=os.environ)
-        load_partitions(event['database_name'],event['table_name'],event['athena_workgroup'])
+        subprocess.run(["spark-submit", "/tmp/spark_script.py", "--batch_date", json.dumps(event["batch_date"])], check=True, env=os.environ)
     except Exception as e :
         logger.error(f'Error Spark-Submit with exception: {e}')
-        logger.info(f'Writing unprocessed_file content to the error file: {event["error_file_key"]}')
-        write_content_to_s3_file(event["error_file_bucket"],event["error_file_key"],os.environ['INPUT_PATHS'])
         raise e
     else:
-        logger.info(f'Script {input_script} successfully submitted')
-        logger.info(f'Deleting error file {event["error_file_key"]}')
-        delete_file_from_s3(event["error_file_bucket"],event["error_file_key"])
+        logger.info(f'Spark job {input_script} successfully completed')
 
 def raise_alert(job_name,error):
     try:
@@ -143,8 +64,8 @@ def raise_alert(job_name,error):
             Destination={
                 "ToAddresses": [
                     "kunal.wadhwa@sportsbaazi.com",
-                    #"gautam.dey@sportsbaazi.com",
-                    #"siddharth.tripathi@sportsbaazi.com"
+                    "gautam.dey@sportsbaazi.com",
+                    "siddharth.tripathi@sportsbaazi.com"
                 ],
             },
             Message={
@@ -177,33 +98,17 @@ def lambda_handler(event, context):
         logger.info("******************Start AWS Lambda Handler************")
         s3_bucket_script = os.environ['SCRIPT_BUCKET']
         input_script = os.environ['SPARK_SCRIPT']
-        database_name = os.environ['DATABASE_NAME']
-        table_name = os.environ['TABLE_NAME']
-        athena_workgroup = os.environ['ATHENA_WORKGROUP']
         
-        #Get the S3 key of file consisting names of unprocessed files from the triggering lambda
-        unprocessed_file_bucket = event["Records"][0]["s3"]["bucket"]["name"]
-        unprocessed_file_key = event["Records"][0]["s3"]["object"]["key"]
+        #Set the S3 key of the batch data that needs to be processed as environment variable
+        os.environ['batch_date'] = event["batch_date"]
         
-        #Get all the unprocessed files as string(each filename in a new line)
-        unprocessed_files = get_unprocessed_files(unprocessed_file_bucket,unprocessed_file_key)
-        
-        #Append error content from error file if it exists
-        unprocessed_files = unprocessed_files + "\n" + read_content_from_s3_file(unprocessed_file_bucket, unprocessed_file_key.replace("unprocessed_file","error_file"))
-        unprocessed_files = unprocessed_files.strip()
-        logger.info(f"Final list of unprocessed_files: {unprocessed_files}")
-        os.environ['INPUT_PATHS'] = unprocessed_files
-    
         #Download Spark script from S3 to local
         s3_script_download(s3_bucket_script,input_script)
         
-        # Set the environment variables for the Spark application
-        event['error_file_bucket'] = unprocessed_file_bucket
-        event['error_file_key'] = unprocessed_file_key.replace("unprocessed_file","error_file")
-        event['database_name'] = database_name
-        event['table_name'] = table_name
-        event['athena_workgroup'] = athena_workgroup
+        #Trigger Spark Job
         spark_submit(s3_bucket_script,input_script, event)
+
+        return { "job_status": "Passed" }
     except Exception as e :
-        raise_alert(table_name,e)
-        raise e
+        #raise_alert(table_name,e)
+        return { "job_status": "Failed"}
